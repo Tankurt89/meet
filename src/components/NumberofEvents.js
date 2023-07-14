2f0c7e823f9adddd12b14262f6e60cb2b52c8466
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-14 14:07:32 -0400

    Number of events
    Number of Events js created to allow the user to enter the max amount of event they would like to se while searching and to allow for proper testing.

---
+++ src/components/NumberofEvents.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const NumberOfEvents = ({ eventNumber, onEventNumberChange }) => {
+  const handleInputChanged = (value) => {
+    onEventNumberChange(value);
+  };
+  console.log(eventNumber);
+
+  return (
+    <div id="number-of-events">
+      <input
+        type="text"
+        className="textbox"
+        placeholder="Enter any Number"
+        value={eventNumber}
+        onChange={(e) => handleInputChanged(e.target.value)}
+      />
+    </div>
+  );
+};
+export default NumberOfEvents;
