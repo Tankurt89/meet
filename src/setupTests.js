f0386bcc95b02cab9af16512e9a4ddfc1d0ed088
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-14 14:05:21 -0400

    Merge error
    Removed lines that were pulled over incorrectly while merging 4.4 branch with master branch.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,15 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-<<<<<<< HEAD
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-
-
-=======
 
 const MESSAGES_TO_IGNORE = [
     "When testing, code that causes React state updates should be wrapped into act(...):",
@@ -25,4 +16,4 @@ console.error = (...args) =>{
     const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
     if (!ignoreMessage) originalError(...args);
 }
->>>>>>> 4.4
+
