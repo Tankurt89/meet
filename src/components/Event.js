da5e900f4c02aa1480040eac5f747bdbdb79c82c
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-14 14:06:43 -0400

    Events
    Events.js created to allow the use of a show details button that changes into a hide details button when clicked while showing/hiding the details. Combined this way to reduce code and make future parts of the project easier.

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,22 @@
-const Event = () => {
+import { useState } from "react"
+
+const Event = ({ event }) => {
+    const [showDetails, setShowDetails] = useState(false)
+    const toggleDetails = () => {
+        setShowDetails(!showDetails)
+    }
     return (
-        <li></li>
+        <li>
+        <div className="event">
+        <div className="location">{event.location} </div>
+        <div className="dateTime">{event.start.dateTime}</div>
+        <div className="name">{event.summary}</div>
+        {showDetails && <div className="description">{event.description}</div>}
+        <button className="details-btn" onClick={toggleDetails}>
+          {showDetails ? "Hide Details" : "Show Details"}
+        </button>
+      </div> 
+        </li>
     )
 }
 
