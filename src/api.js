c5371c5077b9d57a035176c6332e7b2f93bcf637
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-18 11:53:45 -0400

    Try/catch
    try/catch removed from api.js

--- src/api.js
+++ src/api.js
@@ -70,19 +70,12 @@ const removeQuery = () => {
 
 
 const getToken = async (code) => {
-  try{
   const encodeCode = encodeURIComponent(code)
   const response = await fetch(
     'https://ncssdjauvj.execute-api.us-east-2.amazonaws.com/dev/api/token'+ '/' + encodeCode
   )
-  if(!response.ok){
-    throw new Error(`HTTP error! status: ${response.status}`)
-  }
   const { access_token } = await response.json()
   access_token && localStorage.setItem("access_token", access_token)
 
   return access_token
-  }catch (error){
-    error.json()
-  }
 }
