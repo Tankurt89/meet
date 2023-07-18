774535f36588084938e75fe37993df54ce09c5b8
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-17 23:59:55 -0400

    Removed
    Removed mockdata to load on https://tankurt90.

--- src/api.js
+++ src/api.js
@@ -15,7 +15,7 @@ export const extractLocations = (events) => {
 };
 
 export const getEvents = async () => {
-  if (window.location.href.startsWith('http://localhost') || window.location.href.startsWith('https://tankurt89')){
+  if (window.location.href.startsWith('http://localhost')){
     return mockData;
   }
 
@@ -75,13 +75,22 @@ const removeQuery = () => {
   }
 }
 
+
+
 const getToken = async (code) => {
+  try{
   const encodeCode = encodeURIComponent(code)
   const response = await fetch(
     'https://ncssdjauvj.execute-api.us-east-2.amazonaws.com/dev/api/token'+ '/' + encodeCode
   )
+  if(!response.ok){
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
   const { access_token } = await response.json()
   access_token && localStorage.setItem("access_token", access_token)
 
   return access_token
+  }catch (error){
+    error.json()
+  }
 }
