6747f3f7dcb029f381bc3bc98b9469332834c15e
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-24 10:38:35 -0400

    EndToEnd
    First part of the update was editing indentation and overall structure.
    
    Second part was setting up testing for the hide/show details.

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,34 +4,34 @@ describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 250,
-            timeout: 0
-        });
-        page = await browser.newPage();
-        await page.goto('http://localhost:3000/');
-        await page.waitForSelector('.event')
+      browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 50, 
+        timeout: 0 
+      });
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
     });
-
+  
     afterAll(() => {
-        browser.close();
+      browser.close();
     });
-
+  
     test('An event element is collapsed by default', async () => {
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
+      const eventDetails = await page.$('.event .details');
+      expect(eventDetails).toBeNull();
     });
-
+  
     test('User can expand an event to see its details', async () => {
-        await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .details')
-        expect(eventDetails).toBeDefined();
+      await page.click('.event .details-btn');
+      const eventDetails = await page.$('.event .details');
+      expect(eventDetails).toBeDefined();
     });
-
-    test('User can collapse an event to hide details', async () => {
-        await page.click('.event .details-btn');
-        const eventDetails = await page.$('.event .details');
-        expect(eventDetails).toBeNull();
+  
+    test('User can collapse an event to hide its details', async () => {
+      await page.click('.event .details-btn');
+      const eventDetails = await page.$('.event .details');
+      expect(eventDetails).toBeNull();
     });
-})
+});
