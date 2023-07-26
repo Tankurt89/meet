c78cba975881618cf40f5471b3d64f3d721219c8
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-26 07:59:45 -0400

    api
    Added in await on lines 32/34 to try and fix some fetch error received on atatus.

--- src/api.js
+++ src/api.js
@@ -29,9 +29,9 @@ export const getAccessToken = async () => {
   const tokenCheck = accessToken && (await checkToken(accessToken))
 
   if (!accessToken || tokenCheck.error) {
-    localStorage.removeItem("access_token")
+    await localStorage.removeItem("access_token")
     const searchParams = new URLSearchParams(window.location.search)
-    const code = searchParams.get("code")
+    const code = await searchParams.get("code")
     if (!code) {
       const response = await fetch( 
         "https://ncssdjauvj.execute-api.us-east-2.amazonaws.com/dev/api/get-auth-url"
