5223acbe333166c605ab09c412694e6964f44b09
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-07-31 13:39:44 -0400

    Offline
    added coding to allow offline use and getting the info from localStorage

--- src/api.js
+++ src/api.js
@@ -11,6 +11,10 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.online) {
+    const events = localStorage.getItem("lastEvents")
+    return events?JSON.parse(events):[]
+  }
   const token = await getAccessToken()
 
   if (token) {
@@ -19,6 +23,7 @@ export const getEvents = async () => {
     const response = await fetch(url)
     const result = await response.json()
     if (result) {
+      localStorage.setItem("lastEvents", JSON.stringify(result.events))
       return result.events
     } else return null
   }
