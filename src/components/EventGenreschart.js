b5d1186ded54ab622e238c5edc78fdc68459ab51
Tankurt89 <131893494+Tankurt89@users.noreply.github.com>
2023-08-01 14:48:11 -0400

    Percentage
    percentages updated to what the assignment calls for.

--- src/components/EventGenreschart.js
+++ src/components/EventGenreschart.js
@@ -23,17 +23,33 @@ const getData = () => {
       return data.filter((entry) => entry.value > 0);
 };
 
+const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? 'start' : 'end'}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+  
   return (
     <ResponsiveContainer height={400}>
       <PieChart key={data}>
         <Pie
           data={data}
           labelLine={false}
-          outerRadius={outerRadius.current}
+          outerRadius={150}
           dataKey="value"
-          label={({ name, percent }) =>
-            `${name} ${(percent * 100).toFixed(0)}%`
-          }
+          label={renderCustomizedLabel}
         >
           {data.map((_entry, index) => (
             <Cell key={`cell-S${index}`} fill={colors[index]} />
